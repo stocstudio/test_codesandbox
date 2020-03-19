d419234ddeac03789ec6c3837bb3685e37c1d55e
stocstudio <54842972+stocstudio@users.noreply.github.com>
2020-03-19 11:12:40 +0200

    we

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "./styles.css";
+var cdate = new Date().toString();
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
-<div>
+<div><b></b>, 
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
